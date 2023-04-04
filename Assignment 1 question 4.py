6138a557925f5bfc576bb1af686b9903a1cd7319
IAADIPANDEY <129716567+IAADIPANDEY@users.noreply.github.com>
2023-04-04 08:30:23 +0530

    Add files via upload

---
+++ Assignment 1 question 4.py
@@ -0,0 +1,7 @@
+# to get 75 result as string
+num1=25
+num2=int('25')
+num3=int(25.0)
+#using typecasting function a,b,c are converted into integer data type
+result=str(num1+num2+num3)
+print(result)
