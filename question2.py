be203214156be059461054190a581a903819c445
IAADIPANDEY <129716567+IAADIPANDEY@users.noreply.github.com>
2023-04-24 23:48:40 +0530

    Delete question2.py

--- question2.py
+++
@@ -1,9 +0,0 @@
-# Ques 2
-name = "ABC"
-id = "2110XXXX"
-department = "XYZ"
-cgpa = 8.1
-
-output = "Hey, {} Here!\nMy SID is {}\nI am from {} department and my CGPA is {}"
-
-print(output.format(name, id, department, cgpa))
