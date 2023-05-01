ded74ee00049bc043a57c6785c61429ec2439938
IAADIPANDEY <129716567+IAADIPANDEY@users.noreply.github.com>
2023-05-01 22:13:29 +0530

    Assignment 4
    IAADI PANDEY
    22107015

---
+++ Assignment 4 question1.py
@@ -0,0 +1,15 @@
+a= int(input("enter your marks="))
+if a<0 or a>100:
+   print("error")
+elif a<25:
+    print("Grade is E")
+elif 25<=a<45:
+    print ("Grade is E")
+elif 45<=a<50:
+    print("Grade is D")
+elif 50<=a<60:
+    print ("Grade is C")
+elif 60<=a<80:
+    print("Grade is B")
+elif 80<=a:
+    print("Grade is A")
---
+++ Assignment 4 question2.py
@@ -0,0 +1,11 @@
+a = int(input("enter the year="))
+if a<0:
+    print("error")
+elif (a%100==0 and a%400==0):
+    print("It can be a leap year")
+elif (a%100==0):
+    print("It is not be a leap year")
+elif (a%4==0):
+    print("It is a leap year")
+else :
+    print("It is not a leap year")
